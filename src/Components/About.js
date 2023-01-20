26871feab36191840b4e09bf61086bf1d2e30bce
Dagna Schmidt <dagdagi889@gmail.com>
2023-01-20 14:19:20 +0100

    about section done

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import data from '../Data/dataIcons';
 
-const About = () => {
+const About = ( {setIsOpen}) => {
+  useEffect(() =>{
+    setIsOpen(false)
+  }, [])
   return (
-    <div>About</div>
+    <section className='about'>
+      <img className='about__img' src={require('../Images/me.jpeg')} />
+      <div className='about__info'>
+        <div className='about__info__links'>
+          {data.map((item) => {
+            return (
+              <a key={item.id} href={item.link} className='about__info__icon' target={'_blank'}>{item.icon}</a>
+            )
+          })}
+        </div>
+        <p className='about__info__text'>All my photos were taken with PRAKTICA BM.</p>
+      </div>
+    </section>
   )
 }
 
---
+++ src/Data/dataIcons.js
@@ -0,0 +1,24 @@
+import { FaInstagram, FaGithub, FaLinkedinIn } from "react-icons/fa"
+import {BsCodeSlash} from 'react-icons/bs';
+export default [
+    {
+        id: 1,
+        link: "https://www.instagram.com/b.ardzo/",
+        icon: <FaInstagram />,
+    },
+    {
+        id: 2,
+        link: "https://github.com/DagnaSchmidt",
+        icon: <FaGithub />
+    },
+    {
+        id: 3,
+        link: "https://www.linkedin.com/in/dagna-schmidt-90ba37207/",
+        icon: <FaLinkedinIn />
+    },
+    {
+        id: 4,
+        link: "https://dschmidt.netlify.app/",
+        icon: <BsCodeSlash />
+    }
+]
