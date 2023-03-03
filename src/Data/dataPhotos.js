09f14c6f39d8e75ccb4bd41a2b66ad6ee613b1bc
Dagna Schmidt <dagdagi889@gmail.com>
2023-03-03 09:45:54 +0100

    downsize

--- src/Data/dataPhotos.js
+++ src/Data/dataPhotos.js
@@ -1,422 +1,422 @@
 const data = [
     {
         index: 1,
-        url: "p1-1.jpg",
+        url: "p1-1-min.jpg",
         category: "colder / warmer"
     },
     {
         index: 2,
-        url: "p1-2.jpg",
+        url: "p1-2-min.jpg",
         category: "colder / warmer"
     },
     {
         index: 3,
-        url: "p1-3.jpg",
+        url: "p1-3-min.jpg",
         category: "colder / warmer"
     },
     {
         index: 4,
-        url: "p1-4.jpg",
+        url: "p1-4-min.jpg",
         category: "colder / warmer"
     },
     {
         index: 5,
-        url: "p1-5.jpg",
+        url: "p1-5-min.jpg",
         category: "colder / warmer"
     },
     {
         index: 6,
-        url: "p1-6.jpg",
+        url: "p1-6-min.jpg",
         category: "colder / warmer"
     },
     {
         index: 7,
-        url: "p1-7.jpg",
+        url: "p1-7-min.jpg",
         category: "colder / warmer"
     },
     {
         index: 8,
-        url: "p2-1.jpg",
+        url: "p2-1-min.jpg",
         category: "leaves"
     },
     {
         index: 9,
-        url: "p2-2.jpg",
+        url: "p2-2-min.jpg",
         category: "leaves"
     },
     {
         index: 10,
-        url: "p2-3.jpg",
+        url: "p2-3-min.jpg",
         category: "leaves"
     },
     {
         index: 11,
-        url: "p2-4.jpg",
+        url: "p2-4-min.jpg",
         category: "leaves"
     },
     {
         index: 12,
-        url: "p3-1.jpg",
+        url: "p3-1-min.jpg",
         category: "dresden"
     },
     {
         index: 13,
-        url: "p3-2.jpg",
+        url: "p3-2-min.jpg",
         category: "dresden"
     },
     {
         index: 14,
-        url: "p3-3.jpg",
+        url: "p3-3-min.jpg",
         category: "dresden"
     },
     {
         index: 15,
-        url: "p3-4.jpg",
+        url: "p3-4-min.jpg",
         category: "dresden"
     },
     {
         index: 16,
-        url: "p3-5.jpg",
+        url: "p3-5-min.jpg",
         category: "dresden"
     },
     {
         index: 17,
-        url: "p4-1.jpg",
+        url: "p4-1-min.jpg",
         category: "calm / twinge"
     },
     {
         index: 18,
-        url: "p4-2.jpg",
+        url: "p4-2-min.jpg",
         category: "calm / twinge"
     },
     {
         index: 19,
-        url: "p4-3.jpg",
+        url: "p4-3-min.jpg",
         category: "calm / twinge"
     },
     {
         index: 20,
-        url: "p4-4.jpg",
+        url: "p4-4-min.jpg",
         category: "calm / twinge"
     },
     {
         index: 21,
-        url: "p4-5.jpg",
+        url: "p4-5-min.jpg",
         category: "calm / twinge"
     },
     {
         index: 22,
-        url: "p4-6.jpg",
+        url: "p4-6-min.jpg",
         category: "calm / twinge"
     },
     {
         index: 23,
-        url: "p4-7.jpg",
+        url: "p4-7-min.jpg",
         category: "calm / twinge"
     },
     {
         index: 24,
-        url: "p5-1.jpg",
+        url: "p5-1-min.jpg",
         category: "hangman"
     },
     {
         index: 25,
-        url: "p5-2.JPG",
+        url: "p5-2-min.JPG",
         category: "hangman"
     },
     {
         index: 26,
-        url: "p5-3.jpg",
+        url: "p5-3-min.jpg",
         category: "hangman"
     },
     {
         index: 27,
-        url: "p5-4.JPG",
+        url: "p5-4-min.JPG",
         category: "hangman"
     },
     {
         index: 28,
-        url: "p5-5.jpg",
+        url: "p5-5-min.jpg",
         category: "hangman"
     },
     {
         index: 29,
-        url: "p5-6.JPG",
+        url: "p5-6-min.JPG",
         category: "hangman"
     },
     {
         index: 30,
-        url: "p5-7.jpg",
+        url: "p5-7-min.jpg",
         category: "hangman"
     },
     {
         index: 31,
-        url: "p5-8.JPG",
+        url: "p5-8-min.JPG",
         category: "hangman"
     },
     {
         index: 32,
-        url: "p5-9.jpg",
+        url: "p5-9-min.jpg",
         category: "hangman"
     },
     {
         index: 33,
-        url: "p5-10.JPG",
+        url: "p5-10-min.JPG",
         category: "hangman"
     },
     {
         index: 34,
-        url: "p6-1.JPG",
+        url: "p6-1-min.JPG",
         category: "ears of corn"
     },
     {
         index: 35,
-        url: "p6-2.JPG",
+        url: "p6-2-min.JPG",
         category: "ears of corn"
     },
     {
         index: 36,
-        url: "p6-3.jpg",
+        url: "p6-3-min.jpg",
         category: "ears of corn"
     },
     {
         index: 37,
-        url: "p6-4.JPG",
+        url: "p6-4-min.JPG",
         category: "ears of corn"
     },
     {
         index: 38,
-        url: "p6-5.jpg",
+        url: "p6-5-min.jpg",
         category: "ears of corn"
     },
     {
         index: 39,
-        url: "p6-6.JPG",
+        url: "p6-6-min.JPG",
         category: "ears of corn"
     },
     {
         index: 40,
-        url: "p6-7.jpg",
+        url: "p6-7-min.jpg",
         category: "ears of corn"
     },
     {
         index: 41,
-        url: "p6-8.JPG",
+        url: "p6-8-min.JPG",
         category: "ears of corn"
     },
     {
         index: 42,
-        url: "p7-1.JPG",
+        url: "p7-1-min.JPG",
         category: "sicily"
     },
     {
         index: 43,
-        url: "p7-2.JPG",
+        url: "p7-2-min.JPG",
         category: "sicily"
     },
     {
         index: 44,
-        url: "p7-3.JPG",
+        url: "p7-3-min.JPG",
         category: "sicily"
     },
     {
         index: 45,
-        url: "p7-4.JPG",
+        url: "p7-4-min.JPG",
         category: "sicily"
     },
     {
         index: 46,
-        url: "p7-5.JPG",
+        url: "p7-5-min.JPG",
         category: "sicily"
     },
     {
         index: 47,
-        url: "p7-6.JPG",
+        url: "p7-6-min.JPG",
         category: "sicily"
     },
     {
         index: 48,
-        url: "p7-7.JPG",
+        url: "p7-7-min.JPG",
         category: "sicily"
     },
     {
         index: 49,
-        url: "p7-8.JPG",
+        url: "p7-8-min.JPG",
         category: "sicily"
     },
     {
         index: 50,
-        url: "p7-9.JPG",
+        url: "p7-9-min.JPG",
         category: "sicily"
     },
     {
         index: 51,
-        url: "p7-10.JPG",
+        url: "p7-10-min.JPG",
         category: "sicily"
     },
     {
         index: 52,
-        url: "p8-1.JPG",
+        url: "p8-1-min.JPG",
         category: "flow"
     },
     {
         index: 53,
-        url: "p8-2.JPG",
+        url: "p8-2-min.JPG",
         category: "flow"
     },
     {
         index: 54,
-        url: "p8-3.JPG",
+        url: "p8-3-min.JPG",
         category: "flow"
     },
     {
         index: 55,
-        url: "p8-4.JPG",
+        url: "p8-4-min.JPG",
         category: "flow"
     },
     {
         index: 56,
-        url: "p8-5.JPG",
+        url: "p8-5-min.JPG",
         category: "flow"
     },
     {
         index: 57,
-        url: "p9-1.jpg",
+        url: "p9-1-min.jpg",
         category: "trees"
     },
     {
         index: 58,
-        url: "p9-2.JPG",
+        url: "p9-2-min.JPG",
         category: "trees"
     },
     {
         index: 59,
-        url: "p9-3.jpg",
+        url: "p9-3-min.jpg",
         category: "trees"
     },
     {
         index: 60,
-        url: "p9-4.jpeg",
+        url: "p9-4-min.jpeg",
         category: "trees"
     },
     {
         index: 61,
-        url: "p9-5.jpeg",
+        url: "p9-5-min.jpeg",
         category: "trees"
     },
     {
         index: 62,
-        url: "p10-1.jpg",
+        url: "p10-1-min.jpg",
         category: "restless"
     },
     {
         index: 63,
-        url: "p10-2.jpg",
+        url: "p10-2-min.jpg",
         category: "restless"
     },
     {
         index: 64,
-        url: "p10-3.jpg",
+        url: "p10-3-min.jpg",
         category: "restless"
     },
     {
         index: 65,
-        url: "p10-4.jpg",
+        url: "p10-4-min.jpg",
         category: "restless"
     },
     {
         index: 66,
-        url: "p10-5.jpg",
+        url: "p10-5-min.jpg",
         category: "restless"
     },
     {
         index: 67,
-        url: "p8-6.jpeg",
+        url: "p8-6-min.jpeg",
         category: "flow"
     },
     {
         index: 68,
-        url: "p9-6.jpeg",
+        url: "p9-6-min.jpeg",
         category: "trees"
     },
     {
         index: 69,
-        url: "p9-7.jpg",
+        url: "p9-7-min.jpg",
         category: "trees"
     },
     {
         index: 70,
-        url: "p9-8.jpeg",
+        url: "p9-8-min.jpeg",
         category: "trees"
     },
     {
         index: 71,
-        url: "p9-10.jpeg",
+        url: "p9-10-min.jpeg",
         category: "trees"
     },
     {
         index: 72,
-        url: "p9-11.jpeg",
+        url: "p9-11-min.jpeg",
         category: "trees"
     },
     {
         index: 73,
-        url: "p9-12.jpeg",
+        url: "p9-12-min.jpeg",
         category: "trees"
     },
     {
         index: 74,
-        url: "p9-13.jpeg",
+        url: "p9-13-min.jpeg",
         category: "trees"
     },
     {
         index: 75,
-        url: "p9-14.jpeg",
+        url: "p9-14-min.jpeg",
         category: "trees"
     },
     {
         index: 76,
-        url: "p11-1.jpeg",
+        url: "p11-1-min.jpeg",
         category: "rocks"
     },
     {
         index: 77,
-        url: "p11-2.jpeg",
+        url: "p11-2-min.jpeg",
         category: "rocks"
     },
     {
         index: 78,
-        url: "p11-3.jpeg",
+        url: "p11-3-min.jpeg",
         category: "rocks"
     },
     {
         index: 79,
-        url: "p11-4.jpeg",
+        url: "p11-4-min.jpeg",
         category: "rocks"
     },
     {
         index: 80,
-        url: "p11-5.jpeg",
+        url: "p11-5-min.jpeg",
         category: "rocks"
     },
     {
         index: 81,
-        url: "p11-6.jpeg",
+        url: "p11-6-min.jpeg",
         category: "rocks"
     },
     {
         index: 82,
-        url: "p11-8.jpeg",
+        url: "p11-8-min.jpeg",
         category: "rocks"
     },
     {
         index: 83,
-        url: "p11-9.jpeg",
+        url: "p11-9-min.jpeg",
         category: "rocks"
     },
     {
         index: 84,
-        url: "p11-10.jpeg",
+        url: "p11-10-min.jpeg",
         category: "rocks"
     }
 ]
